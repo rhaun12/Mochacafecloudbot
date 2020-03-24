eb43e40f7806d05a783b12b3c865d79e61aaa36e
rhaun12 <62476343+rhaun12@users.noreply.github.com>
2020-03-23 21:45:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,21 +6,17 @@ const token = 'NjkxNzk3MzUxNTM4NDkxNDIz.XnlMuQ.rr6SKF_O4R6udWYUsQTkbzYZNrQ';
 bot.on('ready', () =>{
     console.log('Mocha Bot Online');
 })
-
 bot.on('message', msg=>{
     if(msg.content === "Hello")
         msg.reply("Hello Friend")
 })
-
 bot.on('message', msg=>{
     if(msg.content === "!group")
         msg.reply("Here you go:  https://www.roblox.com/groups/3183170/Mocha-Cafe-Coffee-and-Cream#!/about")
 })
-
 bot.on('message', msg=>{
     if(msg.content === "!robloxgroup")
         msg.reply("Here you go:  https://www.roblox.com/groups/3183170/Mocha-Cafe-Coffee-and-Cream#!/about")
 })
-
 bot.login(token);
 client.login(process.enc.BOT_TOKEN);
